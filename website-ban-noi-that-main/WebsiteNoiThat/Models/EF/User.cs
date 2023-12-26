8a9e2bcfd4d0cae5c587764b75c38c3000471c51
Nguyen Dat <100542888+NguyenDat020403@users.noreply.github.com>
2023-12-26 14:45:09 +0700

    a

---
+++ website-ban-noi-that-main/WebsiteNoiThat/Models/EF/User.cs
@@ -0,0 +1,44 @@
+﻿namespace Models.EF
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Users")]
+    public partial class User
+    {
+        public int UserId { get; set; }
+
+        [StringLength(50)]
+        [DisplayName("Tên")]
+        public string Name { get; set; }
+
+        [StringLength(100)]
+        [DisplayName("Địa chỉ")]
+        public string Address { get; set; }
+
+        [DisplayName("Số điện thoại")]
+        public int? Phone { get; set; }
+
+        [StringLength(50)]
+        [DisplayName("Tên đăng nhập")]
+        public string Username { get; set; }
+
+        [StringLength(50)]
+        [DisplayName("Mật khẩu")]
+        public string Password { get; set; }
+
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        [DisplayName("Nhóm người dùng")]
+        public string GroupId { get; set; }
+
+        [DisplayName("Trạng thái tài khoản")]
+        public bool Status { get; set; }
+    }
+}
